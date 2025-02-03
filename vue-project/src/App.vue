47a8f3c628861c1cd137a9ffd8ccf8186ec1fa65
John Zane Baluyan <jabaluyan@student.apc.edu.ph>
2025-02-03 15:43:20 +0800

    update vue

--- vue-project/src/App.vue
+++ vue-project/src/App.vue
@@ -1,5 +1,19 @@
+<template>
+  <h1>Food</h1>
+  <food-item/><br>
+  <food-item2/><br>
+  <food-item/><br>
+  <personal-profile/><br>
+</template>
+
 <script></script>
 
-<template> <h1>Hello Classmates! My name is Zane.</h1> </template>
-
-<style></style>
+<style>
+  #app > div {
+        border: dashed black 1px;
+        display: inline-block;
+        margin: 10px;
+        padding: 10px;
+        background-color: lightgreen;
+    }
+</style>                  
